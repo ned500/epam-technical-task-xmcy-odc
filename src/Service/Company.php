b887fac3cc7fcfea6307d7dedc176f856786c624
Erno Nagy <erno_nagy@epam.com>
2022-09-19 17:13:10 +0200

    Add CompanyOptionsInterface.php

--- src/Service/Company.php
+++ src/Service/Company.php
@@ -3,10 +3,11 @@
 namespace App\Service;
 
 use App\Exception\WebAccessException;
+use App\Model\CompanyOptionsInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Throwable;
 
-final class Company
+final class Company implements CompanyOptionsInterface
 {
     private array $companyData = [];
 
@@ -24,6 +25,20 @@ final class Company
         }
 
         return $this->companyData;
+    }
+
+    /**
+     * @throws WebAccessException
+     */
+    public function getOptions(): array
+    {
+        $options = [];
+        foreach ($this->getCompanies() as $company) {
+            $options[$company['Company Name']] = $company['Symbol'];
+        }
+        ksort($options);
+
+        return $options;
     }
 
     /**
