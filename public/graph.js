151928887fe347d40cc6192cb3ae00ab120a69eb
Erno Nagy <erno_nagy@epam.com>
2022-09-20 17:26:42 +0200

    Clean-up and fix graph code

---
+++ public/graph.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    const chart = $('#chart');
+    new Chart(chart, {
+        type: 'line',
+        data: {
+            labels: JSON.parse(chart.attr('data-labels')),
+            datasets: [{
+                label: 'Open',
+                data: JSON.parse(chart.attr('data-opens')),
+                backgroundColor: ['rgba(54, 162, 235, 0.2)'],
+                borderColor: ['rgba(54, 162, 235, 0.2)'],
+            }, {
+                label: 'Close',
+                data: JSON.parse(chart.attr('data-closes')),
+                backgroundColor: ['rgba(254, 145, 23, 0.2)'],
+                borderColor: ['rgba(254, 145, 23, 0.2)'],
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    // beginAtZero: true
+                }
+            }
+        }
+    });
+});
