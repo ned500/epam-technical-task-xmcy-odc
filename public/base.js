ed996dc6dd4a353745a87bfead8ccad0e447359a
Erno Nagy <erno_nagy@epam.com>
2022-09-21 10:10:56 +0200

    Add JS check for dates

--- public/base.js
+++ public/base.js
@@ -1,12 +1,29 @@
-$(document).ready(function(){
-    $(".date-fields").datepicker({
+$(document).ready(function() {
+    const dateFields = $(".date-fields");
+    dateFields.datepicker({
         dateFormat: "yy-mm-dd",
     });
 
     const startDate = $("#main_startDate");
     const endDate = $("#main_endDate");
+    $('#main_submit').click(() => {if (dateFields.filter((i, f) => !checkDate(f)).length || new Date(startDate.val()).getTime() > new Date(endDate.val()).getTime()) {
+        alert('Invalid date!')
+       return false;
+    }});
     startDate.datepicker("option", "maxDate", new Date());
     endDate.datepicker("option", "maxDate", new Date());
     startDate.change(() => endDate.datepicker("option", "minDate", startDate.val()));
     endDate.change(() => startDate.datepicker("option", "maxDate", endDate.val()));
 });
+
+const checkDate = (field) => {
+    const dateArray = $(field).val().split('-');
+    const year = Number(dateArray[0]);
+    const month = Number(dateArray[1]) - 1; // JS use month 0-11
+    const day = Number(dateArray[2]);
+    const newDate = new Date(year, month, day);
+    return year === newDate.getFullYear()
+        && month === newDate.getMonth()
+        && day === newDate.getDate()
+        && newDate.getTime() <= new Date().getTime();
+}
--- src/Model/FormData.php
+++ src/Model/FormData.php
@@ -16,12 +16,12 @@ class FormData
     /**
      * @Assert\Range(maxPropertyPath="endDate", maxMessage="This value should be less or equal than End Date.")
      */
-    public DateTimeInterface $startDate;
+    public ?DateTimeInterface $startDate = null;
 
     /**
      * @Assert\Range(minPropertyPath="startDate", max="today", notInRangeMessage="This value should be between Start Date and the current date.")
      */
-    public DateTimeInterface $endDate;
+    public ?DateTimeInterface $endDate = null;
 
     /**
      * @Assert\Email(mode=Email::VALIDATION_MODE_HTML5)
